8e7af415b5bb37c8b3bd7e88d988b24a65594294
sealtx72 <84211791+sealtx72@users.noreply.github.com>
2026-07-27 13:04:54 -0400

    Add demo contact seeding with Bogus and headshot images
    Added Bogus NuGet package for fake data generation. Implemented SeedDemoContactsAsync in DataUtility.cs to seed demo contacts and categories for a demo user, using Bogus and assigning random images. Updated ManageDataAsync to call the new seeding method. Added logic to load demo images from Data/DemoImages/ and included 73 headshot JPEGs for demo contacts. Updated project files accordingly.

--- ContactProBlazorApp/Data/DataUtility.cs
+++ ContactProBlazorApp/Data/DataUtility.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Bogus;
+using ContactProBlazorApp.Models;
+using ContactProBlazorApp.Client.Models.Enums;
+using System.Reflection;
 
 namespace ContactProBlazorApp.Data
 {
@@ -7,15 +11,16 @@ namespace ContactProBlazorApp.Data
     {
         public static async Task ManageDataAsync(IServiceProvider svcProvider)
         {
-            var dbContextSrc = svcProvider.GetRequiredService<ApplicationDbContext>();
+            var dbContextSvc = svcProvider.GetRequiredService<ApplicationDbContext>();
             var userManagerSvc = svcProvider.GetRequiredService<UserManager<ApplicationUser>>();
             var config = svcProvider.GetRequiredService<IConfiguration>();
 
-            await dbContextSrc.Database.MigrateAsync();
+            await dbContextSvc.Database.MigrateAsync();
             await SeedDemoUserAsync(userManagerSvc, config);
+            await SeedDemoContactsAsync(userManagerSvc, dbContextSvc, config);
         }
 
-        public static async Task SeedDemoUserAsync(UserManager<ApplicationUser> userManager, IConfiguration config) 
+        public static async Task SeedDemoUserAsync(UserManager<ApplicationUser> userManager, IConfiguration config)
         {
             try
             {
@@ -47,10 +52,136 @@ namespace ContactProBlazorApp.Data
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Error seeding demo user: {ex.Message}"); 
+                Console.WriteLine($"Error seeding demo user: {ex.Message}");
                 Console.WriteLine(ex.ToString());
                 throw;
             }
+        }
+
+        public static async Task SeedDemoContactsAsync(UserManager<ApplicationUser> userManager, ApplicationDbContext context, IConfiguration config)
+        {
+            string? demoEmail = config["DemoUserLogin"];
+            if (string.IsNullOrEmpty(demoEmail)) return;
+
+            var user = await userManager.FindByEmailAsync(demoEmail);
+            if (user == null)
+            {
+                return;
+            }
+
+            var demoContacts = await context.Contacts
+                .Where(c => c.AppUserId == user.Id)
+                .Include(c => c.Categories)
+                .ToListAsync();
+
+            var demoCategories = await context.Categories
+                .Where(c => c.AppUserId == user.Id)
+                .ToListAsync();
+
+            Random rand = new();
+
+            if (demoContacts.Count == 0)
+            {
+                var newContacts = new Faker<Contact>()
+                    .RuleFor(c => c.LastName, f => f.Name.LastName())
+                    .RuleFor(c => c.Birthdate, f => f.Date.Between(
+                                DateTime.Now - TimeSpan.FromDays(365 * 60),
+                                DateTime.Now - TimeSpan.FromDays(365 * 18)
+                                ))
+                    .RuleFor(c => c.PhoneNumber, f => f.Phone.PhoneNumber())
+                    .RuleFor(c => c.Address1, f => f.Address.StreetAddress())
+                    .RuleFor(c => c.City, f => f.Address.City())
+                    .RuleFor(c => c.State, f => f.PickRandom<State>())
+                    .RuleFor(c => c.ZipCode, f => int.Parse(f.Address.ZipCode("#####")))
+                    .RuleFor(c => c.AppUserId, user.Id)
+                    .Generate(10);
+
+                Faker faker = new();
+
+                var imageDir = Path.Combine(Directory.GetCurrentDirectory(), "Data/DemoImages/");
+                var mensPics = Directory.GetFiles(Path.Combine(imageDir, "Men/")).ToList();
+                var womensPics = Directory.GetFiles(Path.Combine(imageDir, "Women/")).ToList();
+
+                for (int i = 0; i < newContacts.Count; i++)
+                {
+                    Contact contact = newContacts[i];
+
+                    if (i % 2 == 0)
+                    {
+                        contact.FirstName = faker.Name.FirstName(Bogus.DataSets.Name.Gender.Male);
+                        if (mensPics.Count > 0)
+                        {
+                            var pic = mensPics[rand.Next(0, mensPics.Count)];
+                            mensPics.Remove(pic);
+
+                            ImageUpload image = new()
+                            {
+                                Data = await File.ReadAllBytesAsync(pic),
+                                Type = $"image/{Path.GetExtension(pic).TrimStart('.')}"
+                            };
+
+                            contact.Image = image;
+                            context.Images.Add(image);
+                        }
+                    }
+                    else
+                    {
+                        contact.FirstName = faker.Name.FirstName(Bogus.DataSets.Name.Gender.Female);
+                        if (womensPics.Count > 0)
+                        {
+                            var pic = womensPics[rand.Next(0, womensPics.Count)];
+                            womensPics.Remove(pic);
+                            ImageUpload image = new()
+                            {
+                                Data = await File.ReadAllBytesAsync(pic),
+                                Type = $"image/{Path.GetExtension(pic).TrimStart('.')}"
+                            };
+                            contact.Image = image;
+                            context.Images.Add(image);
+                        }
+                    }
+
+                    contact.Email = faker.Internet.Email(contact.FirstName, contact.LastName, "mailinator.com");
+                    if (rand.Next() % 2 == 0)
+                    {
+                        contact.Address2 = new Faker().Address.SecondaryAddress();
+                    }
+
+                    contact.Created = DateTimeOffset.Now;
+                }
+
+                demoContacts.AddRange(newContacts);
+            }
+
+            if (demoCategories.Count == 0)
+            {
+                demoCategories = [
+                    new() { Name= "Family", AppUserId = user.Id },
+                    new() { Name= "Friends", AppUserId = user.Id },
+                    new() { Name= "CoWorkers", AppUserId = user.Id },
+                    new() { Name= "Clients", AppUserId = user.Id },
+                    new() { Name= "School", AppUserId = user.Id },
+                    new() { Name= "Gaming", AppUserId = user.Id },
+                    new() { Name= "Favorites", AppUserId = user.Id },
+                    ];
+
+                context.Categories.AddRange(demoCategories);
+            }
+
+            foreach (var contact in demoContacts.Where(c => c.Categories?.Count == 0))
+            {
+                int numCategories = rand.Next(1, 5);
+                var categories = demoCategories
+                    .OrderBy(c => Guid.NewGuid())
+                    .Take(numCategories);
+
+                contact.Categories = [.. categories];
+                context.Update(contact);
+
+            }
+
+            await context.SaveChangesAsync();
+
         }
     }
 }
